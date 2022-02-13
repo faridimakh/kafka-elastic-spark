ea6bd7ebcdc3a5446e34c5f33ef778ed15e68b80
farid <imakhfaruid@gmail.com>
2022-02-13 13:24:09 +0100

    clean

--- restartdocker.sh
+++ restartdocker.sh
@@ -1,7 +1,2 @@
 #bin/bash
- # shellcheck disable=SC2046
-docker-compose down
-docker rm -f $(docker ps -a -q)
-docker rmi $(docker images -a -q)
-docker volume rm $(docker volume ls -q)
 docker-compose up -d
